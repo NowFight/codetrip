efd642c459eb82e4096870a419830e6b5da41d62
nowfight <rufengemacs@gmail.com>
2015-04-16 14:39:13 +0800

    fix 跳转功能记录资源路径的bug

--- codetrip/facade/src/main/java/org/codetrip/facade/interceptors/JumpTraceInterceptor.java
+++ codetrip/facade/src/main/java/org/codetrip/facade/interceptors/JumpTraceInterceptor.java
@@ -4,21 +4,29 @@ import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.text.SimpleDateFormat;
+import java.util.logging.Logger;
 
 /**
  * Created by RuFeng on 2015/4/16.
  */
 public class JumpTraceInterceptor extends HandlerInterceptorAdapter {
 
+    private final static Logger LOG = Logger.getLogger(JumpTraceInterceptor.class.getName());
+
     /**
-     * 记录
+     * 记录用户的请求路径
      * */
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        if (request.getPathInfo().endsWith("login") || request.getPathInfo().endsWith("register")) {
+        if (request.getPathInfo().endsWith("login") || request.getPathInfo().endsWith("register") ||
+                request.getPathInfo().startsWith("/resources") || request.getPathInfo().startsWith("/resources")) {
             return true;
         }
         request.getSession().setAttribute("preRequestPath", request.getPathInfo());
+        LOG.info(String.format("[%s] request path : %s",
+                new SimpleDateFormat("yyyy/MM/dd hh:mm:ss").format(System.currentTimeMillis()),
+                request.getPathInfo()));
         return true;
     }
 }
